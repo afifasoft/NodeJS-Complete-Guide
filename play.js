c349173b586288002b5fc1b6bb2f84f08ba50aea
afifasoft <abdul.pesit@gmail.com>
2021-01-07 08:26:07 +0530

    Arrow Functions

--- play.js
+++ play.js
@@ -1,14 +1,29 @@
-var name = 'Max';
-var age = 29;
-var hasHobbies = true;
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+// name = 'Maximilian';
 console.log(name);
 console.log(age);
 console.log(hasHobbies);
 
-
-function summarizeUser(userName, userAge, userHasHobby) {
+const summarizeUser = (userName, userAge, userHasHobby) => {
     return 'Name is ' + userName + ', Age is ' + userAge + ' and the user has hobbies ' + userHasHobby;
 }
 
+const add = (a, b) => {
+    return a + b;
+}
 
+const sum = (a, b) => a + b;
+
+const addOne = a => a + 1;
+
+const addRandom = () => 2 + 3;
 console.log(summarizeUser(name, age, hasHobbies));
+console.log(add(10, 30));
+console.log(sum(20, 30));
+console.log(addOne(3));
+console.log(addRandom());
